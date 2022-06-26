d861e06dcfecfa2b8743e0a9c0671e932e8b3415
thawtar <mr.thaw.tar1990@gmail.com>
2022-06-26 17:03:06 +0900

    trying to write it in c++

--- createMesh.c
+++ createMesh.c
@@ -6,6 +6,13 @@ int main()
     printf("===================================================\n\n");
     printf("                 Welcome to foamAuto       \n\n");
     printf("===================================================\n\n");
-
+    char* blockMeshHeader="FoamFile \
+    { \
+        version     2.0; \n \
+        format      ascii; \n \
+        class       dictionary;\n \
+        object      blockMeshDict;\n \
+    }\n";
+    printf("%s", blockMeshHeader);
   return 0;
   }
---
+++ createMesh.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+
+//using namespace std;
+int main(){
+    std::cout << "Hello World\n";
+
+    return 0;
+}
