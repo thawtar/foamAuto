2627877064950372bf1488cffe9b03412e19811f
thawtar <mr.thaw.tar1990@gmail.com>
2022-06-26 09:43:03 +0900

    Update createMesh.c

--- createMesh.c
+++ createMesh.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+  {
+    printf("===================================================\n\n");
+    printf("                 Welcome to foamAuto       \n\n");
+    printf("===================================================\n\n");
+
+  return 0;
+  }
