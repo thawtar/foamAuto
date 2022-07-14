14cad694067b150af5f8955650bf6e7184e75b8d
Thaw Tar <mr.thaw.tar1990@gmail.com>
2022-07-14 13:25:07 +0900

    added boundary conditions and type definitions

--- createCase.hpp
+++ createCase.hpp
@@ -8,6 +8,64 @@
 // this program is to create an OpenFOAM case directory from scratch
 // inputs may include solver name, turbulence on/off, free-surface on/off
 
+// It is convenient to define values of types like fixedValue, zeroGradient or symmetry
+#define empty -1;
+#define fixedValue 0;
+#define zeroGradient 1;
+#define symmetry 2;
+#define noSlip 3;
+#define fixedFluxPressure 4;
+
+// types
+#define uniform 0;
+#define Scalar 0;
+#define Vector 1;
+
+
+struct dimensions
+{
+	int kg;
+	int m;
+	int sec;
+	int Kelvin;
+}typedef dimensions;
+
+struct transportProperties
+{
+	float rho;
+	float nu;
+}typedef transportProperties;
+
+// to store data for each boundary condition for each patch
+struct boundaryCondition
+{
+	std::string name; // name of the boundary
+	int indx;
+	int bcType; // type of boundary condition. fixedValue, zeroGradient or symmetry
+	int valueType; // type of value: scalar or vector
+
+	
+
+};
+
+// It is necessary to create a new instance of this class for each boundary condition file
+class boundaryConditions
+{
+private:
+	int boundaryCount;
+	int bcClass; // whether it is volVectorField, volScalarField
+	std::string objectType;
+};
+
+class constants
+{
+private:
+	int turbulence;
+	int gravity;
+	int transportProperties;
+};
+
+
 class createcase
 {
 private:
